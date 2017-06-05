0c647f8b5286e48fda556aa97c184697af10d187
dregos <dregojevic@gmail.com>
2017-06-05 16:40:23 +0200

    register.php added.

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vivify academy</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1" charset="UTF-8">
+</head>
+	<body class="main-page">
+	<link rel="stylesheet" type="text/css" href="css/vivify.css">
+		<header>
+			<a href="/login.html" title="Login">Login</a>
+			<a href="/register.html" title="Register">Register</a>
+		</header>
+		<main>
+			<h1>Dobrodošao</h1>
+		</main>
+		<footer>
+			<?php 
+				
+				print "<h2>Sva prava zadržana " .gmdate("Y"). "</h2>";
+			?>
+		</footer>
+	</body>
+</html>
